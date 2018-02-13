6004328711337d88bef47b91bdc2f4fcd3fcbe82
Andy Marks <amarks@thoughtworks.com>
2018-02-13 19:32:34 +1100

    No impl change needed for this test to go green

--- spec/components/AppHelper.spec.js
+++ spec/components/AppHelper.spec.js
@@ -10,6 +10,7 @@ describe("The AppHelper module", function() {
     
     it("can complete the necessary fields", function() {
         expect(helper.completeMissingFields([{totalSpeakers: 10, numberOfWomen: 3}])).toEqual([{totalSpeakers: 10, numberOfWomen: 3, numberOfMen: 7, diversityPercentage: .3}]);
+        expect(helper.completeMissingFields([])).toEqual([]);
     });
 
 });
